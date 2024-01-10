2391012b6329b93d48e61a25ad4f477771931e0c
knoxvilledan <138424723+knoxvilledan@users.noreply.github.com>
2024-01-10 12:58:54 -0500

    Add NotFound component

---
+++ src/app/not-found.jsx
@@ -0,0 +1,13 @@
+import Link from "next/link";
+
+const NotFound = () => {
+  return (
+    <div>
+      <h1>Not Found</h1>
+      <p>The page you are looking for does not exist.</p>
+      <Link href="/">Return home</Link>
+    </div>
+  );
+};
+
+export default NotFound;
