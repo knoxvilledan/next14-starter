09ac5c0cb587a52fb368cb1bba5aeb97a0e2c56d
knoxvilledan <138424723+knoxvilledan@users.noreply.github.com>
2024-01-10 19:49:40 -0500

    Update Blog layout and navbar styles.
    Trying to fix Navbar

--- src/components/navbar/links/navLink/navLink.jsx
+++ src/components/navbar/links/navLink/navLink.jsx
@@ -11,7 +11,7 @@ const NavLink = ({ item }) => {
     <Link
       href={item.path}
       className={`${styles.container} ${
-        pathName === item.title && styles.active
+        pathName === item.path && styles.active
       }`}
     >
       {item.title}
