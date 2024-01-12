ede31f0acc8a362bd641d064744494d50d9dfb88
knoxvilledan <138424723+knoxvilledan@users.noreply.github.com>
2024-01-12 10:34:34 -0500

    Add image support and update AboutPage

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,4 +1,17 @@
+import Image from "next/image";
+import styles from "./about.module.css";
+
 const AboutPage = () => {
-  return <div>AboutPage</div>;
+  return (
+    <div>
+      <div className={styles.imgContainer}>
+        <Image
+          src="https://images.pexels.com/photos/19746488/pexels-photo-19746488/free-photo-of-entrance-to-a-closed-amusement-park.jpeg?auto=compress&cs=tinysrgb&w=600"
+          alt=""
+          fill
+        />
+      </div>
+    </div>
+  );
 };
 export default AboutPage;
