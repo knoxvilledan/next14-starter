1a2fae16224d92e8985e59543bdc4d987f2757ec
knoxvilledan <138424723+knoxvilledan@users.noreply.github.com>
2024-01-13 22:17:51 -0500

    Add home module CSS and update page and navbar components

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,28 @@
+import Image from "next/image";
+import styles from "./home.module.css";
+
 const Home = () => {
-  return <div>Homepage</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.textContainer}>
+        <h1>Creative Thoughts Agency</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo, quia
+          voluptatibus. Quisquam, voluptatem. Nihil, quidem. Quisquam,
+        </p>
+        <div className={styles.buttons}>
+          <button className={styles.button}>Learn More</button>
+          <button className={styles.button}>Contact</button>
+        </div>
+        <div className={styles.brands}>
+          <Image src="/brands.png" alt="" fill className={styles.brandIMG} />
+        </div>
+      </div>
+      <div className={styles.imgContainer}>
+        <Image src="/hero.gif" alt="" fill className={styles.heroIMG} />
+      </div>
+    </div>
+  );
 };
 
 export default Home;
