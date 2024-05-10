eb4be3cf676e61abbd97647fd57311419b798881
knoxvilledan <138424723+knoxvilledan@users.noreply.github.com>
2024-05-10 03:37:04 -0400

    chore: Optimize image loading in contact page and update CSS styles

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,13 +1,25 @@
+"useClient";
 import Image from "next/image";
 import styles from "./contact.module.css";
+//import dynamic from "next/dynamic";
+//import HydrationTest from "@/components/hydrationTest";
 
+/*const HydrationTestNoSSR = dynamic(() => import("@/components/hydrationTest"), {
+  ssr: false,
+});*/
 const ContactPage = () => {
+  const a = Math.random();
+
+  console.log(a);
+
   return (
     <div className={styles.container}>
       <div className={styles.imgContainer}>
         <Image src="/contact.png" alt="" fill className={styles.img} />
       </div>
       <div className={styles.formContainer}>
+        {/* <HydrationTestNoSSR />*/}
+        <div suppressHydrationWarning> {a} </div>
         <form action="" className={styles.form}>
           <input type="text" placeholder="First and Last Name" />
           <input type="text" placeholder="Email Address" />
