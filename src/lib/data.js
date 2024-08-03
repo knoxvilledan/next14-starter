2b932ba09217bfc712296bc1ea0bee9139e520cc
knoxvilledan <138424723+knoxvilledan@users.noreply.github.com>
2024-08-02 21:07:36 -0400

    chore: Optimize image loading in contact page and update CSS styles

---
+++ src/lib/data.js
@@ -0,0 +1,47 @@
+import { Post } from "./models";
+import { User } from "./models";
+import { connectToDb } from "./utils";
+
+export const getPosts = async () => {
+  try {
+    connectToDb();
+    const posts = await Post.find();
+    return posts;
+  } catch (err) {
+    console.log(err);
+    throw new Error("Error while fetching posts");
+  }
+};
+
+export const getPost = async (slug) => {
+  try {
+    connectToDb();
+    const post = await Post.findOne({ slug });
+    return post;
+  } catch (err) {
+    console.log(err);
+    throw new Error("Failed while fetching posts");
+  }
+};
+
+export const getUser = async (id) => {
+  try {
+    connectToDb();
+    const user = await User.findById(id);
+    return user;
+  } catch (err) {
+    console.log(err);
+    throw new Error("Error while fetching user");
+  }
+};
+
+export const getUsers = async () => {
+  try {
+    connectToDb();
+    const users = await User.find();
+    return users;
+  } catch (err) {
+    console.log(err);
+    throw new Error("Error while fetching users");
+  }
+};
